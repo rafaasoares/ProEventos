40ed1115b6dcce02f2cd16ca0cdb68f3d2b203bb
Rafael <rafa.asoares@gmail.com>
2021-07-25 16:54:44 -0300

    Criação deo context

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,99 +13,67 @@ namespace ProEventos.API.Controllers
     [Route("[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        public DataContext DataContext { get; }
+
+        public EventoController(DataContext dataContext)
         {
+            this.DataContext = dataContext;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return new Evento[] {
-                new Evento {
-                    EventoId = 1,
-                    Local = "Curitiba",
-                    DataEvento  = "2021-07-25",
-                    Tema  = "Tema",
-                    QtdPessoas  = 10,
-                    Lote  = "Lote",
-                    ImagemURL  = ""
-                },
-                new Evento {
-                    EventoId = 2,
-                    Local = "São Paulo  ",
-                    DataEvento  = "2021-07-25",
-                    Tema  = "Tema",
-                    QtdPessoas  = 10,
-                    Lote  = "Lote",
-                    ImagemURL  = ""
-                }
-            };
+            return DataContext.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetBydId(int id)
+        public Evento GetBydId(int id)
         {
-            return (new Evento[] {
-                new Evento {
-                    EventoId = 1,
-                    Local = "Curitiba",
-                    DataEvento  = "2021-07-25",
-                    Tema  = "Tema",
-                    QtdPessoas  = 10,
-                    Lote  = "Lote",
-                    ImagemURL  = ""
-                },
-                new Evento {
-                    EventoId = 2,
-                    Local = "São Paulo  ",
-                    DataEvento  = "2021-07-25",
-                    Tema  = "Tema",
-                    QtdPessoas  = 10,
-                    Lote  = "Lote",
-                    ImagemURL  = ""
-                }
-            }).Where(x=> x.EventoId == id);
+            return DataContext.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
 
         [HttpPost]
         public Evento Post()
         {
-            return new Evento {
+            return new Evento
+            {
                 EventoId = 1,
                 Local = "Local",
-                DataEvento  = "2021-07-25",
-                Tema  = "Tema",
-                QtdPessoas  = 10,
-                Lote  = "Lote",
-                ImagemURL  = ""
+                DataEvento = "2021-07-25",
+                Tema = "Tema",
+                QtdPessoas = 10,
+                Lote = "Lote",
+                ImagemURL = ""
             };
         }
 
         [HttpPut("{id}")]
         public Evento Put(int id)
         {
-            return new Evento {
+            return new Evento
+            {
                 EventoId = 1,
                 Local = "Local",
-                DataEvento  = "2021-07-25",
-                Tema  = "Tema",
-                QtdPessoas  = 10,
-                Lote  = "Lote",
-                ImagemURL  = ""
+                DataEvento = "2021-07-25",
+                Tema = "Tema",
+                QtdPessoas = 10,
+                Lote = "Lote",
+                ImagemURL = ""
             };
         }
 
         [HttpDelete("{id}")]
         public Evento Delete(int id)
         {
-            return new Evento {
+            return new Evento
+            {
                 EventoId = 1,
                 Local = "Local",
-                DataEvento  = "2021-07-25",
-                Tema  = "Tema",
-                QtdPessoas  = 10,
-                Lote  = "Lote",
-                ImagemURL  = ""
+                DataEvento = "2021-07-25",
+                Tema = "Tema",
+                QtdPessoas = 10,
+                Lote = "Lote",
+                ImagemURL = ""
             };
         }
     }
